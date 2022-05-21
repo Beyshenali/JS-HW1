bd4c01f30945905ce80b1f6db236f23a96b7216d
Beyshenali Imatov <beyshenali.imatov.kk@gmail.com>
2022-05-21 12:37:22 +0600

    JS jana Html

---
+++ JS/script.js
@@ -0,0 +1,45 @@
+// 1-script
+let first = "JS' first lesson";
+
+alert(first);
+
+console.log(first);
+
+// 2-script
+let second = "1-message";
+alert(second);
+console.log(second);
+
+let third = "2-message";
+alert(third);
+console.log(third);
+
+//3-script
+let userName = "Beyshenali";
+let userEmail = "Beyshenali.imatov.kk@gmail.com";
+
+alert(userName);
+alert(userEmail);
+
+console.log(userName);
+console.log(userEmail);
+
+// 4-script
+let weigh1 = "75";
+let weigh2 = "112";
+
+let number1 = prompt("Number of toys you bought?");
+let number2 = prompt("Number of souvenirs you bought?");
+
+alert((result = weigh1 * number1 + weigh2 * number2));
+
+//5-script
+let a = Number1(prompt("Введите число"));
+let b = Number2(prompt("Введите второе число"));
+
+alert(`sum: ${a + b}`);
+alert(`subtraction: ${a - b}`);
+alert(`multiplication: ${a * b}`);
+alert(`division: ${a / b}`);
+alert(`percent: ${a % b}`);
+alert(`degree: ${a ** b}`);
